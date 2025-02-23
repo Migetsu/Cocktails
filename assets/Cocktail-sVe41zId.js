2854bdfdf98286eed0c8f8ad72ce41cfa070ab80
Danil <danilser2005@gmail.com>
2025-02-23 15:02:17 +0500

    Updates

--- assets/Cocktail-sVe41zId.js
+++
@@ -1,2 +0,0 @@
-import{_ as c}from"./index-c3gESAKf.js";const r={};function t(e,n){return null}const o=c(r,[["render",t]]);export{o as default};
-//# sourceMappingURL=Cocktail-sVe41zId.js.map
---
+++ assets/CocktailRandom-Dw7i8Clt.js
@@ -0,0 +1,2 @@
+import{i as c}from"./index-CLIEgfBU.js";const o={};function t(n,r){return" CocktailRandom "}const e=c(o,[["render",t]]);export{e as default};
+//# sourceMappingURL=CocktailRandom-Dw7i8Clt.js.map
