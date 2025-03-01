fb7d2af5f3f4b266f0e06d08946d6c7902f4c314
Danil <danilser2005@gmail.com>
2025-03-01 13:53:58 +0500

    Updates

--- assets/CocktailRandom-Dw7i8Clt.js
+++
@@ -1,2 +0,0 @@
-import{i as c}from"./index-CLIEgfBU.js";const o={};function t(n,r){return" CocktailRandom "}const e=c(o,[["render",t]]);export{e as default};
-//# sourceMappingURL=CocktailRandom-Dw7i8Clt.js.map
